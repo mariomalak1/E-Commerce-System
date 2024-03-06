bda322addeb2cad9a79d227cf9bb28d7ea3a520a
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-03-06 20:53:47 +0200

    done product model

--- Product/admin.py
+++ Product/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Brand, Category, SubCategory
+from .models import Brand, Category, SubCategory, Product, Productmages
 # Register your models here.
 
 admin.site.register(Brand)
 admin.site.register(Category)
 admin.site.register(SubCategory)
+admin.site.register(Product)
+admin.site.register(Productmages)
--- Product/models.py
+++ Product/models.py
@@ -1,12 +1,14 @@
 import datetime
 import os
-
-from PIL import Image
+import shutil
 from io import BytesIO
+from PIL import Image
+
 from django.core.files import File
 from django.db import models
 from django.template.defaultfilters import slugify
 
+from project.settings import BASE_DIR
 # Create your models here.
 
 class Brand(models.Model):
@@ -104,7 +106,7 @@ class SubCategory(models.Model):
 
 class Product(models.Model):
     def uploadProductCoverImagePath(self, filename):
-        return "Products\\" + "\\" + self.product.title + "\\Cover"
+        return "Products\\" + "\\" + self.title + "\\Cover\\" + filename
 
     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=False)
@@ -117,7 +119,7 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     price = models.PositiveIntegerField(default=0)
     imageCover = models.ImageField(upload_to=uploadProductCoverImagePath, null=False, blank=False)
-    ref = models.CharField(max_length=500, null=False, blank=False)
+    ref = models.SlugField(null=False, unique=True, blank=True, allow_unicode=True, max_length=200)
 
     class Meta:
         ordering = ["ref", "created_at", "updated_at"]
@@ -128,13 +130,13 @@ class Product(models.Model):
         if counter == 0:
             return self.title
         elif counter == 1:
-            return self.title + " " + self.created_at.second + " " + self.created_at.min
+            return self.title + " " + str(self.created_at.second) + " " + str(self.created_at.minute)
         elif counter == 2:
-            return self.title + " " + self.created_at.second + "" + self.created_at.min + self.created_at.hour
+            return self.title + " " + str(self.created_at.second) + " " + str(self.created_at.minute) + str(self.created_at.hour)
         elif counter == 3:
-            return self.title + " " + self.created_at.second + "" + self.created_at.min + self.created_at.hour + self.created_at.day
+            return self.title + " " + str(self.created_at.second) + " " + str(self.created_at.minute) + str(self.created_at.hour) + str(self.created_at.day)
         elif counter == 4:
-            return self.title + " " + self.created_at.second + "" + self.created_at.min + self.created_at.hour + self.created_at.day + self.created_at.month + self.created_at.year
+            return self.title + " " + str(self.created_at.second) + " " + str(self.created_at.minute) + str(self.created_at.hour) + str(self.created_at.day) + str(self.created_at.month + self.created_at.year)
 
     def save(self, *args, **kwargs):
         self.created_at = datetime.datetime.now()
@@ -142,7 +144,7 @@ class Product(models.Model):
         # to make unique slugify to ref
         counter = 0
         while (counter + 1):
-            self.ref = slugify(self.__getAllFieldsOfModel())
+            self.ref = slugify(Product.__getFieldsOfModelToSlugify(self, counter))
             product = Product.objects.filter(ref=self.ref).first()
             if product:
                 counter += 1
@@ -150,10 +152,10 @@ class Product(models.Model):
                 counter = -1
 
         # check on the image if it's big will compress it
-        if self.image:
-            if self.image.size > (300 * 1024):
-                new_image = self.compress(self.image)
-                self.image = new_image
+        if self.imageCover:
+            if self.imageCover.size > (300 * 1024):
+                new_image = self.compress(self.imageCover)
+                self.imageCover = new_image
 
         super().save(*args, **kwargs)
 
@@ -167,16 +169,29 @@ class Product(models.Model):
         return new_image
 
     def delete(self, *args, **kwargs):
-        ## to delete the image from server
-        os.remove(self.image.path)
+        try:
+            ## to delete the image from server
+            os.remove(self.imageCover.path)
+        except:
+            pass
+
+        # to delete the other things linked with this object
+        images = Productmages.objects.filter(product=self).all()
+        if images:
+            for image in images:
+                image.delete()
+
+        # to delete all dirs that linked with this object
+        shutil.rmtree(f"{BASE_DIR}\\Media\\Products\\{self.title}")
         super().delete(*args, **kwargs)
+
 
     def __str__(self):
         return self.ref
 
 class Productmages(models.Model):
     def uploadToPath(self, filename):
-        return "Products\\" + "\\" + self.product.title + "\\images"
+        return "Products\\" + "\\" + self.product.title + "\\images\\" + filename
 
     image = models.ImageField(upload_to=uploadToPath)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -201,6 +216,9 @@ class Productmages(models.Model):
         return new_image
 
     def delete(self, *args, **kwargs):
-        ## to delete the image from server
-        os.remove(self.image.path)
+        try:
+            ## to delete the image from server
+            os.remove(self.image.path)
+        except:
+            pass
         super().delete(*args, **kwargs)
